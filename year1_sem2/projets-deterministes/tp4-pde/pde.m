db4ee0a977a515fbe97c88d128a7992b210a8063
Alex Fikl <alexfikl@gmail.com>
2012-04-10 12:16:47 +0200

    projets: started tp5. mostly learning about the problem

---
+++ year1_sem2/projets-deterministes/tp4-pde/pde.m
@@ -0,0 +1,5 @@
+function [ output_args ] = Untitled1( input_args )
+%UNTITLED1 Summary of this function goes here
+%  Detailed explanation goes here
+http://www.colorado.edu/geography/class_homepages/geog_4023_s07/labs/html/PDE_lab.html
+www.public.iastate.edu/~akmitra/aero361/design_web/Laplace.pdf
