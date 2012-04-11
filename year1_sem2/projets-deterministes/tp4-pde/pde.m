60de5a9a488872d79d975bb572656af7f2e8623a
Alex Fikl <alexfikl@gmail.com>
2012-04-11 19:39:21 +0200

    projets: added some docs to the function..starting to get the hang of it

--- year1_sem2/projets-deterministes/tp4-pde/pde.m
+++ year1_sem2/projets-deterministes/tp4-pde/pde.m
@@ -1,6 +1,40 @@
-function [ output_args ] = Untitled1( input_args )
-%UNTITLED1 Summary of this function goes here
-%  Detailed explanation goes here
-http://www.colorado.edu/geography/class_homepages/geog_4023_s07/labs/html/PDE_lab.html
-www.public.iastate.edu/~akmitra/aero361/design_web/Laplace.pdf
-www.fstm.ac.ma/labomac/Taik-cours1_AN3.pdf
+function u = SolvePoisson(f, area, border_func, border_type, N, M)
+    % Simple solver for equations of the type:
+    %   | -(u_{xx} + u_{yy}) = f
+    %   | u = u_0                       (Dirichlet boundary condition)
+    %   | u_n = g                       (Neumann boundary condition)
+    % known as Poisson equations (for f = 0 we have Laplace equations).
+    %
+    % Notation:
+    %   u_xx - second derivative on x
+    %   u_n  - derivative with respect to the normal vector
+    %
+    % Arguments:
+    %   f               given funtion as in the formula.
+    %   area            rectangular area [a, b]x[c, d].
+    %   border_func     4 functions for each border.
+    %   border_type     type of border condition (0 for Dirichlet and 1 for Neumann).
+    %   N               number of discretizations on the x axis
+    %   M               number of discretizations on the y axis
+    %
+    % Returns:
+    %   u               matrix with values approximated at (x_i, y_j)
+    %
+    % Usage:
+    %   u = SolvePoisson(f, area, border_func, border_type, N, M);
+    %
+    % Notes:
+    %   http://web.math.umt.edu/bardsley/courses/412_18/MatlabCode/Poisson2Db.m
+    %   www.public.iastate.edu/~akmitra/aero361/design_web/Laplace.pdf
+    %   www.fstm.ac.ma/labomac/Taik-cours1_AN3.pdf
+    %   http://dissertations.ub.rug.nl/faculties/science/1971/h.j.van.linde/?pLanguage=en&pFullItemRecord=ON
+    %
+    % Copyleft Alexandru Fikl <alexfikl@gmail.com> (c) 2012
+
+    [g1, g2, g3, g4] = border_func;
+
+    if nargin < 6
+        error('Not enough arguments. Use help SolvePoisson.');
+    end
+
+    % loading documentation...
