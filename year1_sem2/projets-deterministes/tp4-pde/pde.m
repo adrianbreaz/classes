3734cb2be43ffea9f6bfe7136600880979d1c22c
Alex Fikl <alexfikl@gmail.com>
2012-05-02 11:29:01 +0200

    projets: deleted everything. going with the matrix thing

--- year1_sem2/projets-deterministes/tp4-pde/pde.m
+++ year1_sem2/projets-deterministes/tp4-pde/pde.m
@@ -34,112 +34,14 @@ function u = SolvePoisson(f, area, border_func, border_type, N, M)
     deltax2 = deltax^2;
     deltay = (area(4) - area(3)) / M;
     deltay2 = deltay^2;
-    h = 2 * deltax2 + 2 * deltay2 - deltax2 * deltay2;
 
-    % MATRIX NOTATION:
-    %   (x_1, y_M)                                              (x_N, y_M)
-    %     1, M                       Top                           N, M
-    %       ------------------------------------------------------
-    %       |                                                    |
-    %       |                                                    |
-    %       |                                                    |
-    %       |                                                    |
-    %  Left |                                                    |  Right
-    %       |                                                    |
-    %       |                                                    |
-    %       |                                                    |
-    %       |                                                    |
-    %       |                                                    |
-    %       ------------------------------------------------------
-    %   1, 1                        Bottom                       N, 1
-    % (x_1, y_1)                                              (x_N, y_1)
-
-    % newly defined functions to make it easier to work with indices
-    fn = @(k, l) f(area(1) + k * deltax, area(3) + l * deltay);
-    gT = @(k, l) border_func(area(1) + k * deltax, area(3) + l * deltay)(1);
-    gR = @(k, l) border_func(area(1) + k * deltax, area(3) + l * deltay)(2);
-    gB = @(k, l) border_func(area(1) + k * deltax, area(3) + l * deltay)(3);
-    gL = @(k, l) border_func(area(1) + k * deltax, area(3) + l * deltay)(4);
-
-    % Fill in the respective borders and starting positions in case of Dirichlet
-    % boundary types
-    % TOP
-    u = zeros(N, M)
-    if border_type[1] == 0
-        u(:, M) = border_func(area(1):deltax:area(2), area(4))(1)
-        ej = M - 1
-    else
-        ej = M
-    end
-
-    % RIGHT
-    if border_type[2] == 0
-        u(N, :) = border_func(area(2), area(3):deltay:area(4))(2)
-        ei = N - 1
-    else
-        ei = N
-    end
-
-    % BOTTOM
-    if border_type[3] == 0
-        u(:, 1) = border_func(area(1):deltax:area(2), area(3))(3)
-        sj = 2
-    else
-        sj = 1
-    end
-
-    % LEFT
-    if border_type[4] == 0
-        u(1, :) = border_func(area(1), area(3):deltay:area(4))(4)
-        si = 2
-    else
-        si = 1
-    end
-
-    % inits
-    u_old = u;
-    epsilon = tol;
-    it = 0;
-
-    while epsilon >= tol && it < it_max
-        for i = si:ei
-            for j = sj:ej
-                % TODO: compute all the border derivatives for Neumann
-                if j == M
-                    % TOP Neumann border
-                    u(i, j) = deltax2 * (u(i, j - 1) + u(i, j + 1));
-                    u(i, j) = u(i, j) + deltay2 * (u(i - 1, j) + u(i + 1, j));
-                    u(i, j) = u(i, j) + deltax2 * deltay2 * fn(i, j);
-                    u(i, j) = u(i, j) / h;
-                elseif i == N
-                    % RIGHT Neumann border
-                    u(i, j) = deltax2 * (u(i, j - 1) + u(i, j + 1));
-                    u(i, j) = u(i, j) + deltay2 * (u(i - 1, j) + u(i + 1, j));
-                    u(i, j) = u(i, j) + deltax2 * deltay2 * fn(i, j);
-                    u(i, j) = u(i, j) / h;
-                elseif j == 1
-                    % BOTTOM Neumann border
-                    u(i, 1) = deltax2 * (u(i, j - 1) + u(i, 2));
-                    u(i, 1) = u(i, j) + deltay2 * (u(i - 1, 1) + u(i + 1, 1));
-                    u(i, 1) = u(i, j) + deltax2 * deltay2 * fn(i, 1);
-                    u(i, 1) = u(i, 1) / h;
-                elseif i == 1
-                    % LEFT Neumann border
-                    u(1, j) = deltax2 * (u(1, j - 1) + u(1, j + 1));
-                    u(1, j) = u(1, j) + deltay2 * (2 * u(2, j) + 2 * deltax * gL(1, j));
-                    u(1, j) = u(1, j) + deltax2 * deltay2 * fn(1, j);
-                    u(1, j) = u(1, j) / h;
-                else
-                    % MIDDLE
-                    u(i, j) = deltax2 * (u(i, j - 1) + u(i, j + 1));
-                    u(i, j) = u(i, j) + deltay2 * (u(i - 1, j) + u(i + 1, j));
-                    u(i, j) = u(i, j) + deltax2 * deltay2 * fn(i, j);
-                    u(i, j) = u(i, j) / h;
-                end
-            end
-        end
-
-        epsilon = norm(u - u_old);
-        u_old = u;
-        it = it + 1;
-    end
+    % function shortcuts
+    gT = @(x, y) border_func(x, y)(1);
+    gR = @(x, y) border_func(x, y)(2);
+    gB = @(x, y) border_func(x, y)(3);
+    gL = @(x, y) border_func(x, y)(4);
+    
+    A = zeros(N * M, N * M);
+    
+ 
+    
