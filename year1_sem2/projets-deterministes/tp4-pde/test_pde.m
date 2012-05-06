39424f0bc4b774c8bfcb5773c2fc0157a9205f1b
Alex Fikl <alexfikl@gmail.com>
2012-05-06 14:09:41 +0200

    projets: finished the construction of the matrix, only for dirichlet

--- year1_sem2/projets-deterministes/tp4-pde/test_pde.m
+++ year1_sem2/projets-deterministes/tp4-pde/test_pde.m
@@ -1,9 +1,10 @@
+format short g
 f = @(x, y) 1;
 border_f = @(x, y) [1 1 1 1];
 border_t = [0 0 0 0];
 
-N = 5;
-M = 5;
+N = 3;
+M = 6;
 area = [-1 1 -1 1];
 
 SolveEq(f, area, border_f, border_t, N, M)
