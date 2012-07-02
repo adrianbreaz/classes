dc76a07fcd2a12ec9dfb2f51ab19a550c0a33ba5
Alex Fikl <alexfikl@gmail.com>
2012-07-02 19:03:32 +0200

    proba: finishing touches on projet1

--- year1_sem2/projets-proba/projet-1/project1.m
+++ year1_sem2/projets-proba/projet-1/project1.m
@@ -20,8 +20,9 @@ f1 = @(x, b) mod(b * x + c, m);             % exercise 1
 f2 = @(x, p) (floor(p * x) + 1) / p;        % exercise 2
 f3 = @(x, b, p) mod(b * x, m);              % exercise 3
 
-disp('Exercise number format: first digit - exercise number');
-disp('second digit - subproblem number');
+disp('Exercise number format:');
+disp(' - first digit - exercise number');
+disp(' - second digit - subproblem number');
 exercise = input('Give exercise number: ');
 
 switch exercise
@@ -36,44 +37,53 @@ switch exercise
         hold on;
         plot(r, 0 * r, 'r*');
         plot([0, m], [0, 0]);
+        title('Linear pseudo-random number generator.');
         hold off;
     case 12
         % Find the period of our random number generator
-        T1 = 0;     % period counter
-        x0 = 0;     % initial value
-        x1 = 1;     % next value
+        T = 0;             % period counter
+        x0 = 0;             % initial value
+        x1 = f1(x0, a);      % next value
 
         while x1 ~= 0
             x1 = f1(x0, a);
             x0 = x1;
-            T1 = T1 + 1;
+            T = T + 1;
         end
 
-        fprintf('The period is: %d\n', T1);
+        fprintf('The period of our generator for ');
+        fprintf('(x0, a, c, m) = (%d, %d, %d, %d) ', x0, a, c, m);
+        fprintf('is: %d\n', T);
     case 14
         % Test the correlation between our PRNG and a uniform distribution.
-        x1 = randi(m - 1, 1, n);
-        x2 = 0 * x1;
-        corrc = zeros(length(1:4:1021), 1);
+        x1 = randi(m - 1, 1, n);                % random variable from {0, .., m-1}
+        x2 = 0 * x1;                            % our test variable
+
+        ad = 1:4:1021;
+        corrc = zeros(length(ad), 1);
         j = 1;
 
-        for a = 1:4:1021
+        for a = ad
             for i = 1:n - 1
                 x2(i + 1) = f1(x2(i), a);
             end
 
-            % corrc(j) = corr(x2, x1);      % octave
-            corr = corrcoef(x1, x2);      % matlab
-            corrc(j) = corr(1, 2);
+            corrc(j) = corr(x2, x1);          % octave
+%              corr = corrcoef(x1, x2);          % matlab
+%              corrc(j) = corr(1, 2);
             j = j + 1;
         end
 
         [minc, ind] = min(abs(corrc));
-        fprintf('best a ever is %d\n', 1 + 4 * (ind - 1));
+        fprintf('best a ever is %d with a corr of %g\n', ad(ind), corrc(ind));
 
-        plot(corrc, 'r*');
+        hold on;
+        plot(ad, corrc, 'r*');
+        plot([0, 1025], [0, 0]);
+        hold off;
+        title('Correlation between a uniform distribution and our linear random number generator.')
     case 21
-        % Test convergence to a U[0, 1] using the discrete uniform distribution
+        % Test convergence to a U[0, 1] using a discrete uniform distribution
         x = 0:0.01:0.99;
 
         hold on;
@@ -86,13 +96,11 @@ switch exercise
     case 33
         % RANDU algorithm
         n = 5000;
-        a = 2^16 + 2 + 1;
+        a = 2^16 + 2 + 1;   % special values for a and m
         m = 2^31;
         x0 = 1;
 
         A = zeros(3, n);
-        x = -10:0.1:10;
-        y = 0 * x;
 
         for i = 1:n
             A(1, i) = f3(x0, a, m);
@@ -103,12 +111,12 @@ switch exercise
 
         A = A / m;
         A = [9 -6 1] * A;
-        cdfplot(A);               % for Octave
+        cdfplot(A);
     case 41
         % Test the Law of Large Numbers that says:
         %   \overline{X}_n \to E[X]
         n = 1024;
-        
+
         xn = randi(m - 1, n, 1);
         xn2 = zeros(n, 1);
         xn = cumsum(xn) ./ [1:n]';
@@ -116,16 +124,18 @@ switch exercise
         for i = 1:n - 1
             xn2(i + 1) = f1(xn2(i), a);
         end
-        
+
         xn2 = cumsum(xn2) ./ [1:n]';
-        
+
         hold on;
         plot([0, n], [m / 2, m / 2], 'LineWidth', 2);
         plot(xn, 'r', 'LineWidth', 2);
         plot(xn2, 'k', 'LineWidth', 2);
+        legend('exact mean', 'our PRNG', 'randi', 'Location', 'SouthEast');
+        title('Convergence to the uniform distribution mean');
         hold off;
     case 43
-        % Compute numerically Var(X) by using n * Var(X_n)
+        % Compute numerically Var(X) where X is generated using our PRNG
         n = 1024;
 
         x = zeros(n, 1);
@@ -139,14 +149,14 @@ switch exercise
         fprintf('approx: Var(X1) = %g\n', var(x));
     case 44
         % Test the central limit theorem
-        n = 2000;
-        p = 2048;
+        n = 128;
+        p = 512;
         nmeanx = n * m / 2;
         nstdx = sqrt(n * (m^2 - 1) / 12);
-        
+
         xhist = zeros(p, 1);
         x = zeros(n, 1);
- 
+
         for i = 1:p
             x(1) = randi(m);
 
@@ -155,15 +165,16 @@ switch exercise
             end
             xhist(i) = (sum(x) - nmeanx) / nstdx;
         end
-        
-        hold on;
-        hist(xhist, 64);
-        hold off;
+
+        histfit(xhist, 64);
     case 50
+        % Test something regarding the Mersenne Twister
         T = 2^19937 - 1;
         mean = 25e+06 * 60 * 60 * 24 * 365;
-        
+
         fprintf('%.16f\n', mean / T);
     otherwise
-        error('Wrong exercise number.\n');
+        fprintf('Wrong exercise number.\n');
+        fprintf('Available exercises are:\n');
+        disp([11 12 14 21 33 41 43 44 50]);
 end
