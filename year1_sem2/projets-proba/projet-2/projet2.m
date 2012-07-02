98884be56f7afd81f447bfb511906de2d567c1fc
Alex Fikl <alexfikl@gmail.com>
2012-07-02 21:45:20 +0200

    proba: more project 2

--- year1_sem2/projets-proba/projet-2/projet2.m
+++ year1_sem2/projets-proba/projet-2/projet2.m
@@ -37,13 +37,13 @@ switch exercise
         hold off;
     case 32
         % Test the Laplace distribution
-        b = 1;
+        lambda = 1;
         mu = 0;
         y = -10:0.01:10;
 
-        cdf1 = @(x) 0.5 * (1 + sign(x - mu) .* (1 - exp(-abs(x - mu)/b)));
-        icdf1 = @(u) mu - b * sign(u - 0.5) .* log(1 - 2 * abs(u - 0.5));
-        pdf1 = @(x) 1 / (2 * b) * exp(-abs(x - mu)/b);
+        cdf1 = @(x) 0.5 * (1 + sign(x - mu) .* (1 - exp(-abs(x - mu)/lambda)));
+        icdf1 = @(u) mu - lambda * sign(u - 0.5) .* log(1 - 2 * abs(u - 0.5));
+        pdf1 = @(x) 1 / (2 * lambda) * exp(-abs(x - mu)/lambda);
 
         X = icdf1(urand);
 
@@ -64,10 +64,10 @@ switch exercise
         y = 0:0.01:3;
 
         cdf1 = @(x) 1 - exp(-lambda * x.^alpha);
-        icdf1 = @(u) - (log(1 - u) / lambda).^(1/alpha);
+        icdf1 = @(u) (-log(1 - u) / lambda).^(1/alpha);
         pdf1 = @(x) alpha * lambda * x.^(alpha - 1) .* exp(-lambda * x.^alpha);
 
-        X = abs(icdf1(urand));
+        X = icdf1(urand);
 
         figure(1)
         subplot(1, 3, 1);
@@ -147,17 +147,114 @@ switch exercise
         plot(y, cdf1(y), 'r-.');
         hold off;
     case 41
-        disp('Not Implemented');
+        % Test the Bernoulli distribution
+        p = 0.7;
+        y = -1:0.01:2;
+
+        cdf1 = @(x) (1 - p) * (x > 0 & x < 1) + (x >= 1);
+        icdf1 = @(u) floor(p + u);
+        pdf1 = @(x) (1 - p) * ( x == 0) + p * (x == 1);
+
+        X = icdf1(urand);
+
+        subplot(1, 3, 1);
+        hist(X / n, 64);
+        subplot(1, 3, 2);
+        plot(y, pdf1(y), 'r', 'LineWidth', 2);
+        subplot(1, 3, 3);
+        hold on;
+        cdfplot(X);
+        plot(y, cdf1(y), 'r-.');
+        hold off;
     case 42
-        disp('Not Implemented');
+        % Test the geometric distribution
+        p = 0.7;
+        y = 1:10;
+
+        cdf1 = @(x) 1 - (1 - p).^x;
+        icdf1 = @(u) ceil(log(1 - u) / log(1 - p));
+        pdf1 = @(x) (1 - p).^(x - 1) * p;
+
+        X = icdf1(urand);
+
+        subplot(1, 3, 1);
+        hist(X / n, 64);
+        subplot(1, 3, 2);
+        hold on;
+        plot(y, pdf1(y), 'r*')
+        plot(y, pdf1(y), 'r', 'LineWidth', 1);
+        hold off
+        subplot(1, 3, 3);
+        hold on;
+        cdfplot(X);
+        plot(y, cdf1(y), 'r*');
+        hold off;
     case 43
-        disp('Not Implemented');
-    case 51
-        disp('Not Implemented');
+        % Test the geometric distribution using the Bernoulli distribution.
+        % TODO
+        p = 0.7;
+        y = 1:0.01:10;
+
+        cdf1 = @(x) 1 - (1 - p).^x;
+        icdf1 = @(u) ceil(log(1 - u) / log(1 - p));
+        pdf1 = @(x) (1 - p).^(x - 1) * p;
+
+        X = icdf1(urand);
+
+        subplot(1, 3, 1);
+        hist(X / n, 64);
+        subplot(1, 3, 2);
+        hold on;
+        plot(1:10, pdf1(1:10), 'r*')
+        plot(y, pdf1(y), 'r', 'LineWidth', 1);
+        hold off
+        subplot(1, 3, 3);
+        hold on;
+        cdfplot(X);
+        plot(y, cdf1(y), 'r*');
+        hold off;
     case 52
-        disp('Not Implemented');
+        p = [0.1 0.1 0.8];
+        pc = cumsum(p);
+        y = 0:0.05:9;
+
+        icdf1 = @(u) 1 * (u <= pc(1)) + 3 * (u <= pc(2) & u > pc(1)) + 8 * (u > pc(2));
+        pdf1 = @(x) p(1) * (x == 1) + p(2) * (x == 3) + p(3) * (x == 8);
+
+        X = icdf1(urand);
+
+        subplot(1, 3, 1);
+        hist(X / n, 64);
+        subplot(1, 3, 2);
+        hold on;
+        plot(y, pdf1(y), 'r', 'LineWidth', 1);
+        hold off
+        subplot(1, 3, 3);
+        hold on;
+        cdfplot(X);
+        hold off;
     case 6
-        disp('Not Implemented');
+        % Test the Poisson distribution
+        lambda = 4;
+        y = 0:20;
+
+        cdf1 = @(x) gammainc(floor(x + 1), lambda) / factorial(floor(x));
+        icdf1 = @(u) poissinv(u, lambda);
+        pdf1 = @(x) lambda.^x ./ factorial(x) * e^(-lambda);
+
+        X = icdf1(urand);
+
+        subplot(1, 3, 1);
+        hist(X / n, 64);
+        subplot(1, 3, 2);
+        hold on;
+        plot(y, pdf1(y), 'r', 'LineWidth', 1);
+        hold off
+        subplot(1, 3, 3);
+        hold on;
+        cdfplot(X);
+        plot(y, cdf1(y), 'r');
+        hold off;
     case 91
         disp('Not Implemented');
     case 92
